3dbe28b18bc46a08dd3642f78aae2c0d13b7a5a5
Sarah <sarah.boening@web.de>
2018-04-10 17:39:19 +0200

    Code clean up, added device features & team document

--- app/src/main/java/com/example/mis/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/mis/helloandroid/MainActivity.java
@@ -24,22 +24,20 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.SocketTimeoutException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
-
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
     ImageView imageView;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.textView);
-        imageView = (ImageView)findViewById(R.id.imageView);
+        imageView = findViewById(R.id.imageView);
+        editText = findViewById(R.id.editText);
     }
 
     // https://stackoverflow.com/questions/5474089/how-to-check-currently-internet-connection-is-available-or-not-in-android
@@ -51,28 +49,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendMessage(View view) {
+
         if (!isNetworkAvailable())
             Toast.makeText(MainActivity.this, "No internet connection", Toast.LENGTH_LONG).show();
+        else {
+            String message = editText.getText().toString();
+            // To make sure the URL is working
+            if (!message.startsWith("http"))
+                message = "http://" + message;
+            new GetPageTask(imageView, textView).execute(message);
 
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        Map<String, String> webpage = new HashMap<String, String>();
-        // To make sure the URL is working
-        if (!message.startsWith("http"))
-            message = "http://" + message;
-        new GetPageTask(imageView, textView).execute(message);
-
-
+        }
     }
-
 
     // inspiration: https://stackoverflow.com/questions/14418021/get-text-from-web-page-to-string
     public class GetPageTask extends AsyncTask<String, Void, String> {
-        private TextView textview;
-        private ImageView imageView;
         Bitmap bm;
         String text;
         Exception exception;
+        private TextView textview;
+        private ImageView imageView;
 
         public GetPageTask (ImageView iv, TextView tv){
             this.imageView = iv;
@@ -85,25 +81,29 @@ public class MainActivity extends AppCompatActivity {
             HttpURLConnection http = null;
             BufferedReader rd = null;
             String type = "";
-            String line = "";
-            Map<String, String > map = new HashMap<String, String>();
+            StringBuffer result = new StringBuffer();
+
             try {
 
                 URL url = new URL(urlString[0]);
                 http = (HttpURLConnection) url.openConnection();
                 http.connect();
                 String contentType = http.getContentType();
-
+                // Check type of content from server response header to switch representation
                 if(contentType.startsWith("image")) {
                     // https://stackoverflow.com/questions/5776851/load-image-from-url
                     bm = BitmapFactory.decodeStream(url.openConnection().getInputStream());
                     type = "image";
                 }
                 else {
+                    String line;
                     rd = new BufferedReader(new InputStreamReader(http.getInputStream()));
 
                     while ((line = rd.readLine()) != null)
-                        text += line;
+                        result.append(line);
+                    text = result.toString();
+                    rd.close();
+                    http.disconnect();
                     if (contentType.contains("text/html"))
                         type = "html";
                     else
@@ -118,36 +118,34 @@ public class MainActivity extends AppCompatActivity {
             } catch (Exception e) {
                 Log.e("AsyncTask", e.getMessage(), e);
                 exception = e;
-            } finally {
-                try {
-                    assert http != null;
-                    http.disconnect();
-                    rd.close();
-                } catch (NullPointerException e) {
-                    Log.e("ConnectClose", e.getMessage(), e);
-                    exception = e;
-                } catch (IOException e) {
-                    Log.e("ReaderClose", e.getMessage(), e);
-                    exception = e;
-                }
-
             }
+
             return type;
         }
 
         @Override
         protected void onPostExecute( String result) {
             if (exception != null)
+                //Show execptions that might have occurred during the http request
                 Toast.makeText(MainActivity.this, exception.getMessage(), Toast.LENGTH_LONG).show();
+            // representation on screen based on content-type
             switch (result) {
                 case "html":
+                    // clear imageview https://stackoverflow.com/questions/2859212/how-to-clear-an-imageview-in-android
+                    imageView.setImageResource(android.R.color.transparent);
+                    textview.setText("");
+                    //makes text scrollable
                     textview.setMovementMethod( new ScrollingMovementMethod());
                     textview.setText(Html.fromHtml(text));
                     break;
                 case "image":
+                    imageView.setImageResource(android.R.color.transparent);
+                    textview.setText("");
                     imageView.setImageBitmap(bm);
                     break;
                 default:
+                    imageView.setImageResource(android.R.color.transparent);
+                    textview.setText("");
                     textview.setText(text);
                     break;
             }
